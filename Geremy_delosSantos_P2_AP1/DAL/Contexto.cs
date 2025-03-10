37597f1f2e63880ff76700082171c6b96257e414
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-03-10 15:00:23 -0400

    Se creo la clase Contexto

---
+++ Geremy_delosSantos_P2_AP1/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Geremy_delosSantos_P2_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Geremy_delosSantos_P2_AP1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public virtual DbSet<Modelo> Modelo { get; set; }
+
+}
